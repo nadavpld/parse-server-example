3b217cb7ce976d52240cb76f5567b5653661e01e
Nadav Peled <nadavpld@gmail.com>
2017-07-01 12:36:32 +0300

    find one

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.get('/api/user', function(request, response) {
   console.log('User ID : ' + userId);
   MongoClient.connect(DB_PATH, function(err, db) {
     collection = db.collection('Users');
-    collection.find({'UserId': userId}, function(err, result){
+    collection.findOne({'UserId': userId}, function(err, result){
       if(err) {
         response.status(400).send('User was not found');
       } else {
